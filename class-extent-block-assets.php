ff1b02a007b5ae9a00b6169398b530d8052d0b05
Elliott Richmond <elliott@squareonemd.co.uk>
2024-10-28 08:49:25 +0000

    simplified filename

--- class-extent-block-assets.php
+++ class-extent-block-assets.php
@@ -2,26 +2,26 @@
 class ExtendBlockAssets {
 
     // Array to hold the block names for which to enqueue assets
-    private $block_assets = [];
+    private $block_assets = array();
 
     /**
      * Constructor function
      * Initializes the block assets (styles) to be enqueued.
      */
-    public function __construct($block_assets = []) {
+    public function __construct( $block_assets = array() ) {
         $this->block_assets = $block_assets;
 
         // Hook into WordPress to register block styles and scripts
-        add_action('init', [$this, 'register_block_assets']);
+        add_action( 'init', array( $this, 'register_block_assets') );
     }
 
     /**
      * Register styles and scripts for the specified blocks
      */
     public function register_block_assets() {
-        foreach ($this->block_assets as $block_name) {
+        foreach ( $this->block_assets as $block_name)  {
             // Register block style with automatically generated handle and path
-            $this->register_and_enqueue_block_style($block_name);
+            $this->register_and_enqueue_block_style( $block_name );
         }
     }
 
@@ -35,17 +35,14 @@ class ExtendBlockAssets {
         if ($style_src) {
             wp_register_style(
                 $style_handle,
-                $style_src,
-                [],
-                null,
-                'all'
+                $style_src
             );
 
             wp_enqueue_block_style(
-                $block_name,
-                [
-                    'handle' => $style_handle,
-                ]
+                'core/' . $block_name,
+                array(
+                    'handle' => $style_handle
+                )
             );
         }
     }
--- example-functions.php
+++ example-functions.php
@@ -2,16 +2,16 @@
 
 function extend_register_enqueue_assets() {
 
-    // To add more assets simply add the 'core/*block-reference*' to the array below
+    // To add more assets simply add the '*block-reference*' to the array below
     $block_assets = [
-        'core/site-logo',
-        'core/heading',
-        'core/image',
-        'core/cover',
-        'core/buttons',
-        'core/button',
-        'core/separator',
-        'core/quote',
+        'site-logo',
+        'heading',
+        'image',
+        'cover',
+        'buttons',
+        'button',
+        'separator',
+        'quote'
     ];
 
     // Instantiate the class with the block assets array
